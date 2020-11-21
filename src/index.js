9ae855e5206277be05fbf3ae5a3ae1dd930b0066
Paweł Piskorz <ppiskorz0@gmail.com>
2020-11-21 20:34:32 +0100

    clean Vue project

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import TablicaKanbanKontener from './TablicaKanbanKontener';
-
-let listaKart = [];
-
-render(<TablicaKanbanKontener />, document.getElementById('root'));
